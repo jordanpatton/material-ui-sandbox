e92c106dab4ffab45b86a823ee3a51f6f4d45e98
Jordan Patton <jordanpatton@gmail.com>
2019-12-12 17:48:26 -0800

    duplicate

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import Box from '@material-ui/core/Box';
 
 import ControlledAutocompleteA from './components/ControlledAutocompleteA';
+import ControlledAutocompleteB from './components/ControlledAutocompleteB';
 
 import './styles.css';
 
 const App: React.FC = () => (
-    <Box style={{ backgroundColor: '#EEEEEE', padding: '20px' }}>
-        <ControlledAutocompleteA />
-    </Box>
+    <>
+        <Box style={{ backgroundColor: '#EEEEEE', padding: '20px' }}>
+            <ControlledAutocompleteA />
+        </Box>
+        <Box style={{ backgroundColor: '#EEEEEE', padding: '20px' }}>
+            <ControlledAutocompleteB />
+        </Box>
+    </>
 );
 
 export default App;
